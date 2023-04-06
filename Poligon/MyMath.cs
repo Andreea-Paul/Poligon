0e123de0c6d586da0d929af378bb42c0d1ef6ed4
Andreea <Andreea@TUF_F15>
2023-04-06 16:50:29 +0300

    Add project files.

---
+++ Poligon/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Poligon
+{
+    public static class MyMath
+    {
+        public static float Distance(PointF A, PointF B)
+        {
+            return (float)Math.Sqrt((A.X - B.X) * (A.X - B.X) + (A.Y - B.Y) * (A.Y - B.Y));
+        }
+    }
+}
